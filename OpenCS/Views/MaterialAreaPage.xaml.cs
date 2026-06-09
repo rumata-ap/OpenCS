a0ebbd5f415f2c6db1e3b44cf742d03a0d81336a
Aleksandr Ponomarev <palexxvlad@yandex.ru>
2026-06-09 15:52:28 +0300

    feat(UI): add MaterialAreaPage with geometry editor and PlotCanvas preview

---
+++ OpenCS/Views/MaterialAreaPage.xaml.cs
@@ -0,0 +1,63 @@
+using CScore;
+using OpenCS.ViewModels;
+using System.Windows.Controls;
+
+namespace OpenCS.Views
+{
+   public partial class MaterialAreaPage : UserControl
+   {
+      MaterialAreaVM _vm;
+
+      public MaterialAreaPage(MaterialArea area, AppViewModel app)
+      {
+         InitializeComponent();
+         _vm = new MaterialAreaVM(area, app);
+         DataContext = _vm;
+         diagramTypeCombo.ItemsSource = System.Enum.GetValues(typeof(DiagrammType));
+         _vm.PropertyChanged += (_, e) =>
+         {
+            if (e.PropertyName == nameof(MaterialAreaVM.PlotElements))
+               UpdatePlot();
+         };
+         _vm.RefreshPlot();
+      }
+
+      void UpdatePlot()
+      {
+         var elements = _vm.PlotElements;
+         if (elements.Count == 0) { preview.Clear(); return; }
+
+         // Вычислить границы из Hull и point-волокон
+         double xMin = double.MaxValue, xMax = double.MinValue;
+         double yMin = double.MaxValue, yMax = double.MinValue;
+
+         var hull = _vm.Model.Hull;
+         if (hull != null && hull.X.Count > 0)
+         {
+            for (int i = 0; i < hull.X.Count; i++)
+            {
+               if (hull.X[i] < xMin) xMin = hull.X[i];
+               if (hull.X[i] > xMax) xMax = hull.X[i];
+               if (hull.Y[i] < yMin) yMin = hull.Y[i];
+               if (hull.Y[i] > yMax) yMax = hull.Y[i];
+            }
+         }
+
+         foreach (var f in _vm.Model.Fibers)
+         {
+            if (f.X - f.Diameter / 2 < xMin) xMin = f.X - f.Diameter / 2;
+            if (f.X + f.Diameter / 2 > xMax) xMax = f.X + f.Diameter / 2;
+            if (f.Y - f.Diameter / 2 < yMin) yMin = f.Y - f.Diameter / 2;
+            if (f.Y + f.Diameter / 2 > yMax) yMax = f.Y + f.Diameter / 2;
+         }
+
+         if (xMin > xMax) { preview.Clear(); return; }
+
+         // Небольшой отступ, если сечение вырожденное
+         if (xMax - xMin < 1e-9) { xMin -= 0.1; xMax += 0.1; }
+         if (yMax - yMin < 1e-9) { yMin -= 0.1; yMax += 0.1; }
+
+         preview.Draw(elements, xMin, xMax, yMin, yMax, squareAxes: true);
+      }
+   }
+}
